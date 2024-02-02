fe59fc8fe60c5a9afd0df5c61af8131aecafbc6c
Nirbhay Malhotra <70895011+nirbhay221@users.noreply.github.com>
2024-02-02 15:07:56 -0500

    RPG - Java - Game Components

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Board newBoard = new Board(8,8);
+
+
+//
+//        newBoard.UserChoiceMove(8,8,'H');
+//        newBoard.initialize(8,8);
+//
+//        newBoard.AssignMarket(64,'M');
+//newBoard.AssignLand(64,'L');
+//        newBoard.AddDefaultPos(8,'H');
+//
+//        System.out.println(" ");
+//newBoard.userMove(8,'H');
+        Logic.StartGame();
+
+
+    }
+
+}
